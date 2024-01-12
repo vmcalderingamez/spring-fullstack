55038f9c8d0ef7e03ca8e96f1e5634e17496a794
LED <vmcalderingamez@gmail.com>
2024-01-12 18:11:35 -0500

    slacking

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
+
     record PingPong(String result){}
     @GetMapping("/ping")
     public PingPong getPingPong(){
